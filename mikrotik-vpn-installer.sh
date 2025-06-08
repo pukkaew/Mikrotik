051b128604bdf63bd6037642da10d11eab569acc
pukkaew <55931016+pukkaew@users.noreply.github.com>
2025-06-08 20:11:45 +0700

    08062025_2011

--- mikrotik-vpn-installer.sh
+++ mikrotik-vpn-installer.sh
@@ -748,9 +748,9 @@ $(cat ta.key)
 </tls-auth>
 key-direction 1
 OVPN_EOF
-OVPN_EOF
 
 echo "Client configuration created: $SYSTEM_DIR/clients/$CLIENT_NAME.ovpn"
+SCRIPT_EOF
 SCRIPT_EOF
 SCRIPT_EOF
 SCRIPT_EOF
